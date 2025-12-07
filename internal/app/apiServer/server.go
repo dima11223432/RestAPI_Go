42043882fd636a2328e75879c7100c392c499b5e
Dima <Sda201018@gmail.com>
2025-12-07 11:52:29 +0300

    add functional for HandleCreate and json tags for User structure

--- internal/app/apiServer/server.go
+++ internal/app/apiServer/server.go
@@ -1,7 +1,9 @@
 package apiserver
 
 import (
+	"RestApi/internal/app/model"
 	"RestApi/internal/app/store"
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -35,6 +37,35 @@ func (s *server) configureRouter() {
 }
 
 func (s *server) HandleUsersCreate() http.HandlerFunc {
+	type request struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
 	return func(w http.ResponseWriter, r *http.Request) {
+		req := &request{}
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			s.logger.Error(w, r, http.StatusBadRequest, err)
+			return
+		}
+		u := model.User{
+			Email:    req.Email,
+			Password: req.Password,
+		}
+		if err := s.store.User().Create(&u); err != nil {
+			s.error(w, r, http.StatusUnprocessableEntity, err)
+		}
+		u.Sanitize()
+		s.respond(w, r, http.StatusCreated, u)
+	}
+}
+func (s *server) error(w http.ResponseWriter, r *http.Request, code int, err error) {
+	s.respond(w, r, code, map[string]string{"error": err.Error()})
+}
+
+func (s *server) respond(w http.ResponseWriter, r *http.Request, code int, data interface{}) {
+	w.WriteHeader(code)
+
+	if data != nil {
+		json.NewEncoder(w).Encode(data)
 	}
 }
--- internal/app/model/user.go
+++ internal/app/model/user.go
@@ -7,10 +7,10 @@ import (
 )
 
 type User struct {
-	ID                int
-	Email             string
-	Password          string
-	EncryptedPassword string
+	ID                int    `json:"id"`
+	Email             string `json:"email"`
+	Password          string `json:"password,omitempty"`
+	EncryptedPassword string `json:"-"`
 }
 
 func (u *User) Validate() error {
@@ -31,6 +31,9 @@ func (u *User) BeforeCreate() error {
 	return nil
 }
 
+func (u *User) Sanitize() {
+	u.Password = ""
+}
 func encryptString(s string) (string, error) {
 	b, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.MinCost)
 	if err != nil {
