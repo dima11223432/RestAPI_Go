724971193bf32acb0dec2e3da558b8c2b6cab883
Dima <Sda201018@gmail.com>
2025-12-09 16:40:53 +0300

    add customized responseWriter class for logger middleware

---
+++ internal/app/apiServer/responseWriter.go
@@ -0,0 +1,13 @@
+package apiserver
+
+import "net/http"
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (w *responseWriter) WriteHeader(code int) {
+	w.statusCode = code
+	w.ResponseWriter.WriteHeader(code)
+}
